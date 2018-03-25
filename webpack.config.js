e822536dca67c2f7725309cec3d8bd8139d41cec
dcb9 <bob@phpor.me>
2018-03-25 20:51:42 +0800

    finished shopping cart, and checkout

--- webpack.config.js
+++ webpack.config.js
@@ -18,17 +18,4 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new BrowserSyncPlugin({
-      // browse to http://localhost:3000/ during development,
-      // ./public directory is being served
-      host: 'localhost',
-      port: 3000,
-      server: {
-        baseDir: ['./'] ,
-        files: ['index.html', 'bundle/app.js'],
-    }
-    }),
-
-  ],
 };
