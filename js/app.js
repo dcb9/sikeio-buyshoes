9cd9eca2c704718faf843312df5fa932d368003f
dcb9 <bob@phpor.me>
2018-03-26 15:45:16 +0800

    adjust mobile layout

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ import "../css/app.css";
 
 window.onload = function() {
   makeCartScrollNicely();
+
+  let $toggle = document.querySelector(".site__right-sidebar-toggle");
+  $toggle.addEventListener("click", function () {
+    document.body.classList.toggle("js-show-right-sidebar");
+  });
   console.log("page loaded");
 }
 
